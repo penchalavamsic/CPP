5ec81d414b5886af008add8bf655a2cdf7912cac
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-03 21:11:19 +0530

    Even or Odd

---
+++ BasicQuestions/Even_Odd.cpp
@@ -0,0 +1,12 @@
+//Even or Odd number
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a number";
+    cin>>a;
+    if(a%2==0)
+        cout<<"Even number";
+    cout<<"Odd number";
+    
+}

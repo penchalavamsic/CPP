337ec63342553020585141e7a6bcbefb94c2a472
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-07 21:25:00 +0530

    Dynamic Array

---
+++ pointers/Dynamic_Array.cpp
@@ -0,0 +1,13 @@
+//Dynamci array
+#include<iostream>
+using namespace std;
+int main(){
+    int *p=new int[5]; //array of 5 integers
+    for(int i=0;i<5;i++){
+        p[i]=i+1;
+    }
+    for(int i=0;i<5;i++){
+        cout<<p[i]<<endl;
+    }
+
+}

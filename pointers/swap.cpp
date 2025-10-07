c2449139ff7437272dd1ecc262df6cfa5795a182
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-07 19:37:04 +0530

    swap using pointers

--- pointers/swap.cpp
+++ pointers/swap.cpp
@@ -1,3 +1,16 @@
 //swapping of numbers using pointers
 #include<iostream>
 using namespace std;
+int swap(int *a, int *b){
+    int temp;
+    //assigning values
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main(){
+    int a=10, b=20;
+    swap(&a, &b);
+    cout<<a<<" "<<b;
+
+}

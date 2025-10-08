a8c2b3fc91af3f38db0fc1461328d2a55a014836
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-10-08 10:47:36 +0530

    area and perimeter of rectangle

---
+++ class_objects/Rectangle.cpp
@@ -0,0 +1,25 @@
+//area and perimeter of rectangle
+#include<iostream>
+using namespace std;
+class Rectangle{
+    public:
+    float length;
+    float breadth;
+    void getDetails(){
+        cout<<"Enter length and breadth"<<endl;
+        cin>>length>>breadth;
+    }
+    float area(){
+        return length*breadth;
+    }
+    float perimeter(){
+        return 2*(length+breadth);
+    }
+};
+int main(){
+    Rectangle r;
+    r.getDetails();
+    cout<<"Area is "<<r.area()<<endl;
+    cout<<"Perimeter is "<<r.perimeter()<<endl;
+    return 0;
+}
